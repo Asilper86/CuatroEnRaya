763bdce5c3cf07e135c797dd435302ee4d42d5c4
asilper <asliper86@gmail.com>
2025-02-01 20:37:25 +0100

    Clase casilla terminado

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -1,4 +1,32 @@
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
 public class Casilla {
+    Ficha ficha;
+
+    public Casilla() {
+        ficha = null;
+    }
+
+    public Ficha getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(Ficha ficha) throws CuatroEnRayaExcepcion {
+        if (ficha == null) {
+            throw new NullPointerException("No se puede poner una ficha nula.");
+        }
+        if (this.ficha != null) {
+            throw new CuatroEnRayaExcepcion("La casilla ya contiene una ficha.");
+        }
+        this.ficha = ficha;
+    }
+
+    public boolean estaOcupada() {
+        return ficha != null;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", (ficha == null) ? " " : ficha.toString());
+    }
 }
