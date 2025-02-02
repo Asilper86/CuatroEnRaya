67bdc302c67ff2fced4920827daea0546a6232aa
asilper <asliper86@gmail.com>
2025-02-02 19:06:29 +0100

    Subida a github 1

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
@@ -1,4 +1,26 @@
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
-public record Jugador() {
+public record Jugador(String nombre, Ficha colorFichas) {
+
+    public Jugardor{
+        validarNombre(nombre);
+        validarColorFichas(colorFichas);
+    }
+
+    private void validarNombre(String nombre){
+        if(nombre == null){
+            throw new NullPointerException("El nombre no puede ser nulo.");
+        }
+
+        if(nombre.trim().isEmpty()){
+            throw new IllegalArgumentException("El nombre no se puede quedar en blanco.");
+        }
+
+    }
+
+    private void validarColorFichas(Ficha colorFichas){
+
+    }
+
+
 }
